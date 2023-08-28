4971dee34f9d7cdc127446b6f8daf8d7b55b3d96
Ivo Nac <inacucchio@fi.uba.ar>
2023-08-28 12:24:48 -0300

    changed approach: trying easyocr, then trying tesseract

--- main.py
+++ main.py
@@ -2,3 +2,9 @@ import easyocr
 import cv2
 from matplotlib import pyplot as plt
 import numpy as np
+
+IMAGE_PATH = 'images/Selection_021.png'
+
+reader = easyocr.Reader(['es'], gpu = False)
+result = reader.readtext(IMAGE_PATH)
+print(result)
