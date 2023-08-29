a8479bf676941f446f0325f096fb0cb33831d2de
Ivo Nac <inacucchio@fi.uba.ar>
2023-08-29 10:10:22 -0300

    tesseract seems to work well enough. Next challenge to find a pattern to tokenize the string obtained or explore methods to obtain data

--- main.py
+++ main.py
@@ -6,18 +6,20 @@ import numpy as np
 
 pytesseract.pytesseract.tesseract_cmd = r'/usr/bin/tesseract'
 
-print(pytesseract.image_to_string(Image.open('images/test.png')))
-print(pytesseract.image_to_boxes(Image.open('images/test.png')))
+object = pytesseract.image_to_string(Image.open('images/test2.png')) 
+print(object)
+print(object[1])
+#print(pytesseract.image_to_boxes(Image.open('images/test2.png')))
 
 # Get verbose data including boxes, confidences, line and page numbers
-print(pytesseract.image_to_data(Image.open('images/test.png')))
+#print(pytesseract.image_to_data(Image.open('images/test2.png')))
 
 # Get information about orientation and script detection
-print(pytesseract.image_to_osd(Image.open('images/test.png')))
+#print(pytesseract.image_to_osd(Image.open('images/test2.png')))
 
 
 # Get HOCR output
-hocr = pytesseract.image_to_pdf_or_hocr('images/test.png', extension='hocr')
+#hocr = pytesseract.image_to_pdf_or_hocr('images/test2.png', extension='hocr')
 
 # Get ALTO XML output
-xml = pytesseract.image_to_alto_xml('images/test.png')
+#xml = pytesseract.image_to_alto_xml('images/test2.png')
